d84d261b2c666efa44a9e76d806c5968db39af9f
雷宇琪 <993302761@qq.com>
2022-10-02 21:14:41 +0800

    加入ribbon

---
+++ Smart_parking_server/Administrators_module/src/main/java/com/example/administrators/config/RibbonRule.java
@@ -0,0 +1,19 @@
+package com.example.administrators.config;
+
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.ZoneAvoidanceRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RibbonRule {
+
+    //通过配置将Ribbon均衡负载策略设置为优先选择相同集群中的实例，失败则访问其他集群
+    @Bean
+    public IRule  LoadRules (){
+        //根据nacos的设置优先选择本地集群
+        return new NacosRule();
+    }
+}
