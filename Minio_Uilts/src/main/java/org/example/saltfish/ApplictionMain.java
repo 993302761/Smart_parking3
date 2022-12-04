7cb4df318eaeca5a05991fe78b565fdc05ed3fc7
saltfile <3205617363@qq.com>
2022-12-04 23:53:45 +0800

    12-04

---
+++ Minio_Uilts/src/main/java/org/example/saltfish/ApplictionMain.java
@@ -0,0 +1,11 @@
+package org.example.saltfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplictionMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplictionMain.class,args);
+    }
+}
